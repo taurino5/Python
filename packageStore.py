77d8b89d88b6a7afbcceb1c7e20de4c21ebd2d04
Taurino <t_t23@txstate.edu>
2019-03-09 17:43:59 -0600

    working Meun and Switch statement

--- packageStore.py
+++ packageStore.py
@@ -1,34 +1,44 @@
 def main():
-	loop = True
-	while loop:
+	while True:
 		call_DisplayMenu()
 print("Exting program, GoodBye")
 		
 
 
 def call_DisplayMenu():
-	print("Welcome to the Package Store.")
-	print("1. Print out the Database")
-	print("2. Add add to the Database")
-	print("3. Delete from the Database")
-	print("4. Search for a package (given its tracking number)")
-	print("5. Show a list of packages withina given weight range")
-	print("6. Exit program, GoodBye")
-	val = input("Please select from the following options above")
+		print("Welcome to the Package Store.")
+		print("1. Print out the Database")
+		print("2. Add add to the Database")
+		print("3. Delete from the Database")
+		print("4. Search for a package (given its tracking number)")
+		print("5. Show a list of packages withina given weight range")
+		print("6. Exit program, GoodBye")
+		call_Validate()
 
+def call_Validate():
+	loop = True
+	while loop:
+		try:
+			val = int(input("Please select from the following options above: "))
+			loop = False
+		except ValueError:
+			print("Invalid Input, try again")
+
+	call_Switch(val)
+
+def call_Switch(val):
 	if val == 1:
 		print("option 1")
 	elif val == 2:
 		print("option 2")
 	elif val == 3:
-		print("option 2")
+		print("option 3")
 	elif val == 4:
 		print("option 4")
 	elif val == 5:
 		print("option 5")
 	elif val == 6:
 		print("option 6")
-		loop = false
 	else:
 		print("Wrong option, please try again")
 
